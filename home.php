a5789397861a86912df448f68bc1de49b118fe2e
eimear maguire <maguirei@tcd.ie>
2013-03-29 23:30:02 +0000

    hiding password

--- home.php
+++ home.php
@@ -87,8 +87,10 @@
 			<div style="padding-top:10px;padding-left:100px;float:middle">
 
 					<?php
-					// Create connection			
-					$con=mysqli_connect("k.tfa.ie","disney","kandy","website");
+					include "details.php";
+					// Create connection
+					
+					$con=mysqli_connect($host,$logname,$pass, $db);
 					// Check connection
 						if (mysqli_connect_errno($con))
 						{
