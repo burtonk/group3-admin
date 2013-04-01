a6335fcc19b07698e6229cfd65a4e524b3b87c86
eimear maguire <maguirei@tcd.ie>
2013-04-01 15:55:26 +0100

    stockLevel rebuild
    restore updateStock

--- updateStock.php
+++ updateStock.php
@@ -1,5 +1,13 @@
 <?php
-	
+	// Create connection
+	$con=mysqli_connect("k.tfa.ie","disney","kandy", "website");
+
+	// Check connection
+		if (mysqli_connect_errno($con))
+		{
+		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+
 	$sql="INSERT INTO product (Stock_Level)
 	VALUES
 	('$_POST[Stock_Level]')";
@@ -10,4 +18,5 @@
 	}
 	echo "1 record added";
 
+	mysqli_close($con);
 ?>
