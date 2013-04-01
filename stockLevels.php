061a6d276d29653163c511edd826e34042d951b5
eimear maguire <maguirei@tcd.ie>
2013-04-01 14:47:52 +0100

    stockLevel rebuild
    connect to MySQL

--- stockLevels.php
+++ stockLevels.php
@@ -9,8 +9,25 @@
 	<?php include 'header.php'; include 'sidebar.php' ?>
 	<div id="content-wrapper">
 		<div id="content">
+			<h2>Stock levels</h2>
+			<p> Hellooo?</p>
+			
+			<?php
+				// Create connection					
+				$con=mysqli_connect("k.tfa.ie","disney","kandy", "website");
 
-			<p> Hellooo?</P>
+				// Check connection
+					if (mysqli_connect_errno($con))
+					{
+					  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+					}
+					
+					else {
+						echo "connected to db";
+					}
+					
+					mysqli_close($con);
+			?>	
 	
 		</div>
 	</div>
