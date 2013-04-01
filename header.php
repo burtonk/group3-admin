51fb5913626e9adfba2126e5bf8dfb8d493e5db4
eimear maguire <maguirei@tcd.ie>
2013-04-01 03:39:10 +0100

    content styling edit

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 		<META http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<link href="style.css" rel="stylesheet" type="text/css">
 	</head>
-	-->
-	<body>
+	
+	<body>-->
 		<div id="header-wrapper">
 			<div id="header">
 				<a href="home.php"><img src="logo.gif" alt = "Gradinata Admin" color="white" class = "head-pic"></a>
