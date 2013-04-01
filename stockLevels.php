b6f0fbe4f2a2756c6e3535597901168c6de7630d
eimear maguire <maguirei@tcd.ie>
2013-04-01 14:39:52 +0100

    reduce stockLevels

--- stockLevels.php
+++ stockLevels.php
@@ -2,60 +2,10 @@
 <html>
 	<head>
 	<META http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link href="style.css" rel="stylesheet" type="text/css">
 	</head>
 
 	<body>
-	<?php include 'header.php'; include 'sidebar.php' ?>
-	
-	<div id="content-wrapper">
-		<div id="content">
-			<h2>Stock levels</h2>
-			
-				<?php
-					
-					// Create connection
-					
-					$con=mysqli_connect("k.tfa.ie","disney","kandy", "website");
-					// Check connection
-						if (mysqli_connect_errno($con))
-						{
-						  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-						}
-							$result = mysqli_query($con,"SELECT * FROM product");
+	<p> Hellooo?</P>
+	</body>
 
-							echo "<table border='1'>
-							<tr>
-							<th>Name</th>
-							<th>Product I.D</th>
-							<th>Price</th>
-							<th>Weight</th>
-							<th>Stock Level</th>
-							<th>Minimum Level</th>
-
-							</tr>";
-							while($row = mysqli_fetch_array($result))
-							  {
-							  echo "<tr>";
-							  echo "<td>" . $row['Name'] . "</td>";
-							  echo "<td>" . $row['P_Id'] . "</td>";
-							  echo "<td>" . $row['Price'] . "</td>";
-							  echo "<td>" . $row['Weight'] . "</td>";
-							  echo "<td>" . $row['Stock_Level'] . "</td>";
-							  echo "<td>" . $row['Min_Level'] . "</td>";
-							  echo "<td>	<form action="updateProduct.php" method="post">
-											New Level: <input type="text" name="Stock_Level"></li>
-											<input type="submit" value="Save"></td>"
-							  echo "</tr>";
-							  }
-							echo "</table>";
-
-							mysqli_close($con);
-							?>
-							
-			
-		</div>
-	</div>
-
-</body>
 </html>
