42150905f723be12684dfb03b248404052c04e3a
eimear maguire <maguirei@tcd.ie>
2013-04-01 23:09:29 +0100

    database

--- currentOrders.php
+++ currentOrders.php
@@ -13,78 +13,78 @@
 				<h2>Currently Processing Orders </h2>
 					<?php
 					
-					// Create connection
-					
+					// Create connection					
 					$con=mysqli_connect("k.tfa.ie","disney","kandy", "website");
 					// Check connection
-						if (mysqli_connect_errno($con))
-						{
-						  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+					if (mysqli_connect_errno($con))
+					{
+					  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+					}
+					
+					$result = mysqli_query($con,"SELECT * FROM the_order WHERE Progress <4");
+
+					echo "<table border='1'>
+					<tr>
+					<th>Email</th>
+					<th>Order I.D</th>							
+					<th>Product items</th>
+					<th>Total Price</th>
+					<th>Date</th>
+					<th>Progress</th>
+					<th>Update</th>
+					</tr>";
+					while($row = mysqli_fetch_array($result))
+					  {
+					  
+					  /*progress: no. -> text $row = mysqli_fetch_array($result)*/
+					  $progNo=$row['Progress'];
+					  $progTable=mysqli_query($con,"SELECT * FROM progress_options WHERE Progress_Id=$progNo");
+					  $progress= mysqli_fetch_array($progTable);
+
+					 /*product item fixer*
+					$order = $row['OrderID'];						 
+					$items = mysqli_query($con, "SELECT	* FROM order_item WHERE Order_Id=$order");		*/
+					
+
+					  echo "<tr>";
+					  echo "<td>" . $row['Email'] . "</td>";
+					  echo "<td>" . $row['OrderID'] . "</td>";							  
+					  
+					  echo "<td>";
+					  while ($getItems = mysqli_fetch_array($items)){
+						echo "item" . $getItems['Name'] . " ";
 						}
-							$result = mysqli_query($con,"SELECT * FROM the_order WHERE Progress <4");
+					  echo "</td>";
+					  
+					  echo "<td>" . $row['Total_Price'] . "</td>";
+					  echo "<td>" . $row['Date1'] . "</td>";
+					  
+					  
+					  /* FROM HERE!!!!*/
+								  
+								  
+					
+					  
+					 echo "<td>start cell";
+					 echo "<br> progress is " . $progress['Name'] . "<br>";
+					 echo " end cell</td>";
+					  
+					  /*/echo "<td>" . $row['Progress'] . "</td>";
+					  /*echo "<td><button type = 'button'>Update</button></td>"*/
+					  echo "</tr>";
+					  }
+					echo "</table>";
 
-							echo "<table border='1'>
-							<tr>
-							<th>Email</th>
-							<th>Order I.D</th>							
-							<th>Product items</th>
-							<th>Total Price</th>
-							<th>Date</th>
-							<th>Progress</th>
-							<th>Update</th>
-							</tr>";
-							while($row = mysqli_fetch_array($result))
-							  {
-							  
-							  /*progress: no. -> text*/
-							  $prog=$row['Progress'];
-							  $progStatus=mysqli_query($con,"SELECT * FROM progress_options WHERE Progress_Id=$prog");
-								$progress=$progStatus['Name'];
-
-							 /*product item fixer*/
-							$order = $row['OrderID'];						 
-							$items = mysqli_query($con, "SELECT	* FROM order_item WHERE Order_Id=$order");		
-							
-
-							  echo "<tr>";
-							  echo "<td>" . $row['Email'] . "</td>";
-							  echo "<td>" . $row['OrderID'] . "</td>";							  
-							  
-							  echo "<td>";
-							  while ($getItems = mysqli_fetch_array($items)){
-								echo "item" . $getItems['Name'] . " ";
-								}
-							  echo "</td>";
-							  
-							  echo "<td>" . $row['Total_Price'] . "</td>";
-							  echo "<td>" . $row['Date1'] . "</td>";
-							  
-							  
-							  /* FROM HERE!!!!*/
-							  			  
-										  
-							
-							  
-							 echo "<td>start cell";
-							 echo "<br> progress is " . $progress . "<br>";
-							 echo " end cell</td>";
-							  
-							  /*/echo "<td>" . $row['Progress'] . "</td>";
-							  /*echo "<td><button type = 'button'>Update</button></td>"*/
-							  echo "</tr>";
-							  }
-							echo "</table>";
-
-							mysqli_close($con);
-							?>
-							<br>
-							<br>
-							<br>
-							<!--<form action="emailCustomer.php" method="post">
-								Customer to email: <input type="text" name="Email">
-								Progress: <input type="text" name="Progress">
-								<input type="submit" value="Send email">
-							</form>-->
+					mysqli_close($con);
+					?>
+					<br>
+					<br>
+					<br>
+					<!--<form action="emailCustomer.php" method="post">
+						Customer to email: <input type="text" name="Email">
+						Progress: <input type="text" name="Progress">
+						<input type="submit" value="Send email">
+					</form>-->
 		</div>
 	</div>
 
