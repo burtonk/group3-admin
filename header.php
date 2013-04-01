d5339c9ae6637521ef18f2edc5c371a0eb23112f
eimear maguire <maguirei@tcd.ie>
2013-04-01 02:02:07 +0100

    separate header and sidebar files

---
+++ header.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<META http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	
+	<body>
+		<div id="header-wrapper">
+			<div id="header">
+				<h1> Gradinata Admin Page </h1>
+			</div>	
+		</div>
+	<body>
+</html>
--- home.php
+++ home.php
@@ -6,34 +6,10 @@
 	</head>
 
 	<body>
-		<div id="header-wrapper">
-			<div id="header">
-				<h1> Gradinata Admin Page </h1>
-			</div>	
-		</div>
-
-	<div id="bar-wrapper">
-		<div id="bar">
-			<ul>
-			<a href="home.php"><li>Home</li></a>
-				<li>Manage orders<ul>
-					<a href="currentOrders.php"><li>Orders being processed</li></a>
-					<a href="accountDetails.php"><li>Customer account details</li></a>
-					<a href="pastOrders.php"><li>Past orders</li></a>
-					<a href="reviews.php"><li>Customer Reviews</li></a>
-					
-			</ul>
-
-				</li>
-				<li>Manage Stocks<ul>
-					<a href="productCatalogue.php"><li>Product catalogue</li></a>
-					<a href="stockLevels.php"><li>Stock levels</li></a>
-					<a href="addProduct.php"><li>Add product</li></a>
-					<a href="inactiveStock.php"><li>Inactive stock</li></a>
-				</li>
-			</ul>
-		</div>
-	</div>
+	
+	<?php include "header.php"?>
+	<?php include "sidebar.php"?>
+		
 
 	
 	<div id="content-wrapper">
