884e2238064c8f934b614ac57efdcca8f23047cd
eimear maguire <maguirei@tcd.ie>
2013-04-01 01:17:33 +0100

    login edit1

--- home.php
+++ home.php
@@ -44,7 +44,7 @@
 			<div style="padding-top:10px;padding-left:100px;float:middle">
 
 					<?php
-					include "/root/config/details.php";
+					include "root/config/details.php";
 					// Create connection
 					
 					$con=mysqli_connect($host,$logname,$pass, $db);
@@ -87,7 +87,7 @@
 			<div style="padding-top:10px;padding-left:100px;float:middle">
 
 					<?php
-					include "details.php";
+					include "root/config/details.php";
 					// Create connection
 					
 					$con=mysqli_connect($host,$logname,$pass, $db);
