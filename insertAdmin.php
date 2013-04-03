3bc15825d3c676a076159c007bc1d53cca99d748
eimear maguire <maguirei@tcd.ie>
2013-04-03 01:02:41 +0100

    flesh out admin page

--- insertAdmin.php
+++ insertAdmin.php
@@ -1,3 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+	<META http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+	
+	<?php
+	// Connects to your Database
+	mysql_connect("k.tfa.ie", "disney", "kandy") or die(mysql_error());
+	mysql_select_db("website") or die(mysql_error());
+
+	//Checks if there is a login cookie
+	if(isset($_COOKIE['ID_my_site'])){
+	//if there is, it logs you in and directes you to the members page
+		$username = $_COOKIE['ID_my_site']; 
+		$pass = $_COOKIE['Key_my_site'];
+		$check = mysql_query("SELECT * FROM the_user WHERE Email = '$username' AND Admin_status = '1' ")or die(mysql_error());
+
+		while($info = mysql_fetch_array( $check )) {
+			if ($pass != $info['Password']){
+				header("Location: login.php");
+			}
+			else{
+				//do nothing
+			}
+		}
+	 }
+	 else {
+		header("Location: login.php");
+	  }
+	?>
+	
+	<?php include 'header.php'; include 'sidebar.php' ?>
+
+	
+	<div id="content-wrapper">
+		<div id="content">
+
 <?php 
 // Connects to your Database 
 mysql_connect("k.tfa.ie", "disney", "kandy") or die(mysql_error()); 
@@ -72,3 +112,9 @@ mysql_select_db("website") or die(mysql_error());
 <?php
 }
 ?> 
+
+</div>
+	</div>
+
+</body>
+</html>
